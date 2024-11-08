a3066c30df59e4d36bbc1f6378fbc33b44ef4eaf
Markus Chiarot <cmarkus@hotmail.ch>
2024-11-08 16:00:32 +0100

    add signal example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { AsyncPipe, DatePipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { Select } from '@ngxs/store';
+import { Component, inject } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import {
   DoctorState,
@@ -19,4 +19,10 @@ export class AppComponent {
   @Select(DoctorState.medicalLicense) license$!: Observable<MedicalLicense>;
   @Select(DoctorStateQueries.fullLicense) fullLicense$!: Observable<string>;
   @Select(DoctorStateQueries.postal) postal$!: Observable<string>;
+
+  private store = inject(Store);
+
+  license = this.store.selectSignal(DoctorState.medicalLicense);
+  fullLicense = this.store.selectSignal(DoctorStateQueries.fullLicense);
+  postal = this.store.selectSignal(DoctorStateQueries.postal);
 }
